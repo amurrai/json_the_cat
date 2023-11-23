b985b75a3c7c1369f14ade93fd5b1ebb7091efc0
amurrai <andre.murrai@gmail.com>
2023-11-22 22:22:46 -0500

    add comments and lint code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
 
-const url = "https://api.theceatapi.com/v1/breeds/" 
+const url = "https://api.thecatapi.com/v1/breeds/" 
 const breed = process.argv[2]; 
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
+  // request error handling
+  if (error) { 
+    console.log(error);
   }
-
+  // parse JSON string into an object
   const data = JSON.parse(body);
+  // search for input through the data array
   const found = data.find(({ name }) => name === breed);
+  // handle case where breed is not found
   if (!found) {
     console.log("Breed not found!");
     return
